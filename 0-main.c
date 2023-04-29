143bfcbec4b7bd893454e00de0c417c6a7666244
AbebeTeferi Lemma <abebeteferi.teferi@gmail.com>
2023-04-29 22:53:01 +0300

    Add files via upload

---
+++ 0-main.c
@@ -0,0 +1,20 @@
+#include "print_array.c"
+#include "buuble_sort.c"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
